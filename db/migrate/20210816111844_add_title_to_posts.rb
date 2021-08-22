937736bccde09531d18b9c293a53d6be53f5503e
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-22 16:55:26 +0800

    change db to postgres for heroku deployment

--- db/migrate/20210816111844_add_title_to_posts.rb
+++ db/migrate/20210816111844_add_title_to_posts.rb
@@ -1,6 +1,6 @@
 class AddTitleToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_column :posts, :title, :string, null: false
+    add_column :posts, :title, :text, null: false
     rename_column :posts, :post, :body
     change_column_null :posts, :body, false
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_08_16_111844) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.string "username"
